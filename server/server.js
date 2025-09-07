9c5a24aa8749bc5bcfead60efc87e3c708c5edbe
hax <56984134+habtamu-tamere@users.noreply.github.com>
2025-09-07 14:07:11 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
@@ -22,4 +22,5 @@ app.use('/api/influencers', influencerRoutes);
 app.use(express.static('../client')); // Serve frontend
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
